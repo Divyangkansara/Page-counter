562e6182d873e47f60f01f7153057bb3e3927839
Divyang <divyang.kansara@technostacks.com>
2024-02-13 18:05:28 +0530

    Done

---
+++ mycount/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    ct = request.session.get('count', 0)
+    new_count = ct + 1
+    request.session['count'] = new_count
+    return render(request, 'mycount/home.html', {'c': new_count})
+
